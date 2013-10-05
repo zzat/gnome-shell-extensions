df05a685083900d325ea2761defc07260fe90428
Tarun Kumar Joshi <tj.joshi7@gmail.com>
2013-10-06 02:21:17 +0530

    Add a visualizer at bottom right corner
    A pressure visualizer is added at bottom right corner which increases in
    size as pressure increases indicating pressure applied at the corner.

--- message_tray_on_bottom_right_corner@zzat/extension.js
+++ message_tray_on_bottom_right_corner@zzat/extension.js
@@ -1,14 +1,60 @@
 
+const Lang = imports.lang;
 const Meta = imports.gi.Meta;
+const St = imports.gi.St;
 
 const LayoutManager = imports.ui.main.layoutManager;
+const Tweener = imports.ui.tweener;
 
+//Modified Jasper's tray pressure visualizer
+//to show visualizer at bottom right corner 
+const PressureViz = new Lang.Class({
+    Name: 'PressureViz',
+
+    _init: function(threshold) {
+        this.actor = new St.Widget({ style_class: 'pressure-viz',
+                                     opacity: 0});
+        this._threshold = threshold;
+        this.value = 0;
+        this.x = LayoutManager.trayBox.get_width();
+    },
+
+    get value() {
+        return this._value;
+    },
+
+    set value(value) {
+        value = Math.min(value, this._threshold);
+
+        this._value = value;
+        let percent = value / this._threshold;
+
+        this.actor.opacity = percent * 255;
+        let translationFactor = percent * 150;
+        this.actor.height = translationFactor;
+        this.actor.width = translationFactor;
+        this.actor.x = this.x - translationFactor;
+        this.actor.translation_x = translationFactor / 2;
+        this.actor.translation_y = -(translationFactor / 2);
+    },
+
+    destroy: function() {
+        this.actor.destroy();
+    },
+});
+
+let trayBarrierHitId = 0;
+let trayPressureViz = null;
+let origReset = null;
 
 function init() {
 }
 
-
 function enable() {
+    let pressure = LayoutManager._trayPressure;
+
+    trayPressureViz = new PressureViz(pressure._threshold);
+    LayoutManager.trayBox.add_child(trayPressureViz.actor);
 
     LayoutManager._trayBarrier_ = null;
 
@@ -46,12 +92,40 @@ function enable() {
     };
 
     LayoutManager._updateTrayBarrier();
-}
 
+    trayBarrierHitId = LayoutManager._trayBarrier.connect('hit', function() {
+        if (pressure._isTriggered)
+            return;
+
+        Tweener.removeTweens(trayPressureViz);
+        trayPressureViz.value = pressure._currentPressure;
+        Tweener.addTween(trayPressureViz, { time: pressure._timeout / 1000,
+                                            onUpdate: function() {
+                                                pressure._lastTime = global.display.get_current_time_roundtrip();
+                                                trayPressureViz.value = pressure._currentPressure;
+                                                pressure._trimBarrierEvents();
+                                            } });
+    });
+
+    origReset = pressure._reset;
+    pressure._reset = function() {
+        origReset();
+        Tweener.removeTweens(trayPressureViz);
+        Tweener.addTween(trayPressureViz, { value: 0, time: 0.5 });
+    };
+}
 
 function disable() {
     
     //reset everything
+    
+    let pressure = Main.layoutManager._trayPressure;
+    trayPressureViz.destroy();
+    trayPressureViz = null;
+    pressure._reset = origReset;
+    origReset = null;
+    Main.layoutManager._trayBarrier.disconnect(trayBarrierHitId);
+    trayBarrierHitId = 0;
 
     if (this._trayBarrier_) {
         this._trayPressure.removeBarrier(this._trayBarrier_);
